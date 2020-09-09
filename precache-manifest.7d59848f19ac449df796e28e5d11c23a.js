1ea4b5b6f58738a4122a94e8d00a7a069acba481
INOCcent-afk <michael828inoc@gmail.com>
2020-09-09 11:54:46 +0800

    Updates

---
+++ precache-manifest.7d59848f19ac449df796e28e5d11c23a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4106e4cf44d73802a85317b190222ff9",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "4730e78b542a46fb3040",
+    "url": "/recipe-app/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "b5ba15c5c28934f55ad5",
+    "url": "/recipe-app/static/js/2.4b37d867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.4b37d867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4730e78b542a46fb3040",
+    "url": "/recipe-app/static/js/main.5b70a07c.chunk.js"
+  },
+  {
+    "revision": "cd77bb1956273d314d45",
+    "url": "/recipe-app/static/js/runtime-main.14680ac4.js"
+  }
+]);
